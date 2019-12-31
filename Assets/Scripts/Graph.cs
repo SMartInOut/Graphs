97cfa2579f6946dece8631cdb85eccbc376c8f9f
SMartInOut <martin.jurklies@gmx.de>
2019-12-31 14:41:39 +0100

    Restructure graph generating methods and implement collider for edges

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Graph : MonoBehaviour
@@ -7,16 +6,15 @@ public class Graph : MonoBehaviour
 	#region - Properties -
 	public GameObject VertexPrefab;
 	public GameObject EdgePrefab;
-	public int VertexCount = 10;
-	public int EdgeCount = 30;
+	public List<Vertex> Vertices;
+	public List<Edge> Edges;
+	public int VertexCount => Vertices.Count;
+	public int EdgeCount => Edges.Count;
 	#endregion
 	
 	#region - Private fields -
 	private Vertex selectedVertex;
-	private List<Vertex> vertices;
-	
 	private Edge selectedEdge;
-	private List<Edge> edges;
 	#endregion
 	
 	public bool IsStronglyConnected()
@@ -29,165 +27,71 @@ public class Graph : MonoBehaviour
 		return false;
 	}
 	
-	public IEnumerator TraverseBFS(Vertex root)
+	public IEnumerable<(Vertex Vertex, Vertex Predecessor, int Distance)> TraverseBFS(Vertex root)
 	{
-		Queue<Vertex> queue = new Queue<Vertex>(root.Neighbours);
+		var queue = new Queue<Vertex>();
+		var visited = new bool[VertexCount];
+		var bfsInfos = new List<(Vertex Vertex, Vertex Predecessor, int Distance)>();
+		
+		var rootIndex = Vertices.IndexOf(root);
+		visited[rootIndex] = true;
+		bfsInfos.Add((root, null, 0));
+		queue.Enqueue(root);
 		
 		while (queue.Count > 0)
 		{
-			var next = queue.Dequeue();
-			foreach (var neighbour in next.Neighbours)
+			var current = queue.Dequeue();
+			foreach (var neighbour in current.Neighbours)
 			{
-				if (!queue.Contains(neighbour))
+				var neighbourIndex = Vertices.IndexOf(neighbour);
+				if (!visited[neighbourIndex])
 				{
+					visited[neighbourIndex] = true;
+					bfsInfos.Add((neighbour, current, bfsInfos.Find(info => info.Vertex == current).Distance + 1));
 					queue.Enqueue(neighbour);
 				}
 			}
 		}
-
-		return null;
+		
+		return bfsInfos;
 	}
-	
-	#region - Initialization -
-	private void GenerateRandomGraph()
-	{
-		GenerateVertices();
-		GenerateEdges();
-		
-		Camera.main.orthographicSize = VertexCount * 1.1f;
-	}
-	
-	private void GenerateVertices()
-	{
-		vertices = new List<Vertex>(VertexCount);
-		
-		GameObject verticesGO = new GameObject("Vertices");
-		verticesGO.transform.parent = transform;
-		
-		float anglePerVertex = 2 * Mathf.PI / VertexCount;
-		
-		for (int i = 0; i < VertexCount; i++)
-		{
-			var vertexPosition = VertexCount * new Vector2(Mathf.Sin(anglePerVertex * i), Mathf.Cos(anglePerVertex * i));
-			
-			GameObject vertexObject = Instantiate(VertexPrefab, vertexPosition, Quaternion.identity);
-			vertexObject.name = "Vertex" + i;
-			vertexObject.transform.parent = verticesGO.transform;
-			
-			Vertex vertex = vertexObject.GetComponent<Vertex>();
-			vertex.ID = i;
-			vertex.Label = i.ToString();
-			vertex.Weight = 0;
-			vertex.OnSelectVertex += SelectVertex;
-			
-			vertices.Add(vertex);
-		}
-	}
-	
-	private void GenerateEdges()
-	{
-		edges = new List<Edge>(EdgeCount);
-		
-		GameObject edgesGO = new GameObject("Edges");
-		edgesGO.transform.parent = transform;
-		
-		System.Random prng = new System.Random();
-		
-		for (int i = 0; i < EdgeCount; i++)
-		{
-			int sourceIndex;
-			int targetIndex;
-			
-			// Retry to get a new edge by getting a new source and target if the edge with chosen source and target already exists (no multigraph)
-			do
-			{
-				sourceIndex = prng.Next(0, vertices.Count);
-				targetIndex = prng.Next(0, vertices.Count);
-				
-				// Retry to get target if source and target are the same vertex (no loops)
-				while (sourceIndex == targetIndex)
-				{
-					targetIndex = prng.Next(0, vertices.Count);
-				}
-			}
-			while (edges.Exists(e => e.Source == vertices[sourceIndex] && e.Target == vertices[targetIndex]));
-			
-			var source = vertices[sourceIndex];
-			var target = vertices[targetIndex];
-			var edgeEndPoints = new Vector3[] { source.transform.position, target.transform.position };
-			var edgePosition = edgeEndPoints[0] + (edgeEndPoints[1] - edgeEndPoints[0]) / 2;
-			
-			GameObject edgeObject = Instantiate(EdgePrefab, edgePosition, Quaternion.identity);
-			edgeObject.name = "Edge" + sourceIndex + "-" + targetIndex;
-			edgeObject.GetComponent<LineRenderer>().SetPositions(edgeEndPoints);
-			edgeObject.transform.parent = edgesGO.transform;
-			
-			Edge edge = edgeObject.GetComponent<Edge>();
-			edge.ID = i;
-			edge.Source = source;
-			edge.Target = target;
-			edge.Label = i.ToString();
-			edge.Weight = 0;
-			edge.OnSelectEdge += SelectEdge;
-			
-			edges.Add(edge);
-			
-			source.Neighbours.Add(target);
-			source.OutDegree++;
-			target.InDegree++;
-		}
-		
-		/*foreach (var edge in edges)
-		{
-			if (edges.Exists(e => e.Source == edge.Target && e.Target == edge.Source))
-			{
-				var edgeLR = edge.GetComponent<LineRenderer>();
-				var edgeDirection = (edgeLR.GetPosition(1) - edgeLR.GetPosition(0)).normalized;
-				edgeLR.positionCount = 5;
-				edgeLR.SetPosition(4, edgeLR.GetPosition(1));
-				edgeLR.useWorldSpace = false;
-				edgeLR.SetPosition(1, Vector3.right * 0.4f + edgeDirection * 0.25f);
-				edgeLR.SetPosition(2, Vector3.right);
-				edgeLR.SetPosition(3, Vector3.right * 0.4f + edgeDirection * 0.25f);
-				edgeLR.useWorldSpace = true;
-			}
-		}*/
-	}
-	#endregion
 	
 	#region - Selection -
-	private void SelectVertex(Vertex vertex)
+	public void SelectVertex(Vertex vertex)
 	{
 		selectedVertex = vertex;
 		selectedEdge = null;
-		Debug.Log(selectedVertex.GetComponent<Vertex>().Label + " selected");
+		Debug.Log("Vertex " + selectedVertex.Label + " selected");
 	}
 	
-	private void SelectEdge(Edge edge)
+	public void SelectEdge(Edge edge)
 	{
 		selectedEdge = edge;
 		selectedVertex = null;
-		Debug.Log(selectedEdge.GetComponent<Edge>().Label + " selected");
+		Debug.Log("Edge " + selectedEdge.Label + " selected");
 	}
 	#endregion
 	
 	#region - MonoBehaviour methods -
 	private void Reset()
 	{
-		VertexCount = 10;
-		EdgeCount = 30;
 	}
 	
 	// Start is called before the first frame update
 	private void Start()
     {
-		GenerateRandomGraph();
+		
     }
 	
     // Update is called once per frame
     private void Update()
     {
-        
+        //if (Input.OnMouseButtonDown(0))
+		{
+			// if mouse hits collider of vertex v
+			// drag v to mouseposition
+			// edges have to change accordingly
+		}
     }
 	#endregion
 }
---
+++ Assets/Scripts/GraphManager.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GraphManager : MonoBehaviour
+{
+	public Graph Graph;
+
+	public int VertexCount = 10;
+	public int EdgeCount = 30;
+	
+	#region - Initialization -
+	public void GenerateRandomGraph(bool isDirected)
+	{
+		GenerateVertices();
+		GenerateEdges(isDirected);
+	}
+	
+	private void GenerateVertices()
+	{
+		Graph.Vertices = new List<Vertex>(VertexCount);
+		
+		GameObject verticesGO = new GameObject("Vertices");
+		verticesGO.transform.parent = Graph.transform;
+		
+		float anglePerVertex = 2 * Mathf.PI / VertexCount;
+		
+		// Arrange the vertices in a circle
+		for (int i = 0; i < VertexCount; i++)
+		{
+			var vertexPosition = VertexCount * new Vector2(Mathf.Sin(anglePerVertex * i), Mathf.Cos(anglePerVertex * i));
+			GenerateVertex(i, i.ToString(), vertexPosition).transform.parent = verticesGO.transform;
+		}
+	}
+
+	private GameObject GenerateVertex(int id, string label, Vector3 position)
+	{
+		GameObject vertexObject = Instantiate(Graph.VertexPrefab, position, Quaternion.identity);
+		vertexObject.name = "Vertex" + id;
+		
+		Vertex vertex = vertexObject.GetComponent<Vertex>();
+		vertex.ID = id;
+		vertex.Label = label;
+		vertex.Weight = 0;
+		vertex.OnSelectVertex += Graph.SelectVertex;
+		
+		Graph.Vertices.Add(vertex);
+
+		return vertexObject;
+	}
+
+	private void GenerateEdges(bool isDirected)
+	{
+		Graph.Edges = new List<Edge>(EdgeCount);
+		
+		GameObject edgesGO = new GameObject("Edges");
+		edgesGO.transform.parent = Graph.transform;
+		
+		System.Random prng = new System.Random();
+		
+		for (int i = 0; i < (isDirected ? EdgeCount : EdgeCount * 2); i += (isDirected ? 1 : 2))
+		{
+			int sourceIndex;
+			int targetIndex;
+			
+			// Retry to get a new edge by getting a new source and target if the edge with chosen source and target already exists (no multigraph)
+			do
+			{
+				sourceIndex = prng.Next(0, Graph.Vertices.Count);
+				targetIndex = prng.Next(0, Graph.Vertices.Count);
+				
+				// Retry to get target if source and target are the same vertex (no loops)
+				while (sourceIndex == targetIndex)
+				{
+					targetIndex = prng.Next(0, Graph.Vertices.Count);
+				}
+			}
+			while (Graph.Edges.Exists(e => e.Source == Graph.Vertices[sourceIndex] && e.Target == Graph.Vertices[targetIndex]
+										|| (!isDirected && e.Source == Graph.Vertices[targetIndex] && e.Target == Graph.Vertices[sourceIndex])));
+			
+			var source = Graph.Vertices[sourceIndex];
+			var target = Graph.Vertices[targetIndex];
+			GenerateEdge(source, target, i, i.ToString()).transform.parent = edgesGO.transform;
+
+			if (!isDirected)
+			{
+				GenerateEdge(target, source, i+1, i+1.ToString()).transform.parent = edgesGO.transform;
+			}
+		}
+
+		/*foreach (var edge in edges)
+		{
+			if (edges.Exists(e => e.Source == edge.Target && e.Target == edge.Source))
+			{
+				var edgeLR = edge.GetComponent<LineRenderer>();
+				var edgeDirection = (edgeLR.GetPosition(1) - edgeLR.GetPosition(0)).normalized;
+				edgeLR.positionCount = 5;
+				edgeLR.SetPosition(4, edgeLR.GetPosition(1));
+				edgeLR.useWorldSpace = false;
+				edgeLR.SetPosition(1, Vector3.right * 0.4f + edgeDirection * 0.25f);
+				edgeLR.SetPosition(2, Vector3.right);
+				edgeLR.SetPosition(3, Vector3.right * 0.4f + edgeDirection * 0.25f);
+				edgeLR.useWorldSpace = true;
+			}
+		}*/
+	}
+
+	private GameObject GenerateEdge(Vertex source, Vertex target, int id, string label)
+	{
+		var edgeEndPoints = new Vector3[] { source.transform.position, target.transform.position };
+		var edgePosition = edgeEndPoints[0] + (edgeEndPoints[1] - edgeEndPoints[0]) / 2;
+
+		// LineRenderer and EdgeCollider2D work with local positions, therefore the edgeEndPoints have to be recalculated from world to local coordinates
+		var edgeEndPointsLocal = new Vector3[] { edgeEndPoints[0] - edgePosition, edgeEndPoints[1] - edgePosition };
+		
+		GameObject edgeObject = Instantiate(Graph.EdgePrefab, edgePosition, Quaternion.identity);
+		edgeObject.name = "Edge" + source.ID + "-" + target.ID;
+		var lineRenderer = edgeObject.GetComponent<LineRenderer>();
+		lineRenderer.SetPositions(edgeEndPointsLocal);
+		var edgeCollider = edgeObject.GetComponent<EdgeCollider2D>();
+		edgeCollider.points = edgeEndPointsLocal.Select(eep => (Vector2)eep).ToArray();
+		
+		Edge edge = edgeObject.GetComponent<Edge>();
+		edge.ID = id;
+		edge.Source = source;
+		edge.Target = target;
+		edge.Label = label;
+		edge.Weight = 0;
+		edge.OnSelectEdge += Graph.SelectEdge;
+		
+		Graph.Edges.Add(edge);
+		
+		source.Neighbours.Add(target);
+		source.OutDegree++;
+		target.InDegree++;
+
+		return edgeObject;
+	}
+	#endregion
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.cyan;
+		foreach (var edge in Graph.Edges)
+		{
+			if (edge != null)
+			{
+				Gizmos.DrawLine(edge.transform.TransformPoint(edge.GetComponent<EdgeCollider2D>().points[0]), edge.transform.TransformPoint(edge.GetComponent<EdgeCollider2D>().points[1]));
+			}
+		}
+	}
+	
+	// Start is called before the first frame update
+	private void Start()
+	{
+		Graph = Instantiate(Graph, transform.position, Quaternion.identity);
+		GenerateRandomGraph(true);
+		var output = Graph.TraverseBFS(Graph.Vertices[0]);
+		foreach (var item in output)
+		{
+			Debug.Log("(" + item.Vertex.Label + ", " + (item.Predecessor?.Label ?? "null") + ", " + item.Distance + ")");
+		}
+
+		Camera.main.orthographicSize = Graph.VertexCount * 1.1f;
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		
+	}
+}
